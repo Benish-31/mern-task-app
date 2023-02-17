6f54739a2dd237b053f913d295edccca6832f065
Benito Ishimwe <73600412+Benish-31@users.noreply.github.com>
2023-02-17 17:30:50 +0100

    Update App.js

--- taskapp/src/frontend/src/App.js
+++ taskapp/src/frontend/src/App.js
@@ -2,7 +2,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import TaskList from "./components/TaskList";
 
-export const URL = "https://app-task.onrender.com/";
+export const URL = "http://localhost:5000";
 
 function App() {
   return (
