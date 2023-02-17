5f24cae0d4e38f78429233ad97e7f575bd57ce81
Ishimwe <boie@gft.com>
2023-02-17 16:15:23 +0100

    Add backend url

--- taskapp/src/frontend/src/App.js
+++ taskapp/src/frontend/src/App.js
@@ -2,7 +2,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import TaskList from "./components/TaskList";
 
-export const URL = "http://localhost:5000";
+export const URL = "https://app-task.onrender.com/";
 
 function App() {
   return (
